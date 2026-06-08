bfb8aaaa20b949b6e294fc2cd990fcf9c40ae442
Sofi <sofipal72@gmail.com>
2026-06-08 11:06:24 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit=0;
+        for(int i=0; i<prices.size(); i++){
+            for(int j=i+1; j<prices.size(); j++){
+                int profit = prices[j]-prices[i];
+                maxProfit=max(profit, maxProfit);
+            }
+        }
+        return maxProfit;
+    }
+};
